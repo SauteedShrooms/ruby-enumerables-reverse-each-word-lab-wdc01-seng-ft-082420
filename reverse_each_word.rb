29c55b9fb6facd9f3bcff02285fb9a6a91480d33
Nicholas Baccete <nickbaccete@gmail.com>
2020-08-20 23:05:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,5 +2,5 @@ require 'pry'
 
 def reverse_each_word(string)
   new_array = string.split(/ /)
-  new_array.collect {|element| element.reverse}
+  new_array.collect {|element| element.reverse}.join
 end
